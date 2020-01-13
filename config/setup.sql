2d37af5d450a1d5e2f135c56549cb93308185b62
Ryan Wilson <rwilson@ucvts.org>
2020-01-13 08:09:25 -0500

    fixed typo in assignment_grades create statement

--- config/setup.sql
+++ config/setup.sql
@@ -875,7 +875,7 @@ CREATE TABLE IF NOT EXISTS assignment_grades (
     points_possible INTEGER NOT NULL,
     is_graded INTEGER NOT NULL,
     FOREIGN KEY (course_id) REFERENCES courses (course_id),
-    FOREIGN KEY (assignment_id) REFERENCES assignemnts (assignment_id),
+    FOREIGN KEY (assignment_id) REFERENCES assignments (assignment_id),
     FOREIGN KEY (student_id) REFERENCES students (student_id),
     PRIMARY KEY (course_id, assignment_id, student_id)
 );
